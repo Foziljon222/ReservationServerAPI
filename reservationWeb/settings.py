1c863b4fdcda1b957de6ac3be1893f346c96fbae
Foziljon <safikovfoziljon@gmail.com>
2024-11-22 15:34:06 +0500

    changed settings to deploy

--- reservationWeb/settings.py
+++ reservationWeb/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'reservationWeb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backend.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
         # 'ENGINE': 'django.db.backends.postgresql',
         # 'NAME': 'ReservationApi',
